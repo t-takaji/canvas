35e0c7339e95b61452805544ced5e3b696accb2b
t-takaji <42364759+t-takaji@users.noreply.github.com>
2022-03-07 23:58:29 +0900

    Create web.php

---
+++ web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::post('ajax_file_upload', 'UsersController@uploadFile')->name('ajax_file_upload');
